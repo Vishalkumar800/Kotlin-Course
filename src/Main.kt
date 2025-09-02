e2da40278c1a9fe0c8b3ae9d057f79cf903b47f0
Vishal <statusb130@gmail.com>
2025-09-02 12:35:25 +0530

    Lecture First

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+/**
+ * Kotlin is programming language and it is statically type language
+ * val ----  val cannot be reassigned
+ * var --- var , its value can be reassigned
+ */
+
+fun main(){
+    var age = 20
+    println("My age is $age")
+    age = 50
+    println("My age is $age")
 }
+
